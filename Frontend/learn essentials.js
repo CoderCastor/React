c2701c705cc5fbedfe9caaf13f96e8649b9e0de6
codercastor <tejasshinde935@gmail.com>
2024-08-09 18:48:06 +0530

    react restarted

---
+++ Frontend/learn essentials.js
@@ -0,0 +1,10 @@
+//we cannot changes datatype directly
+//like 
+var arr = [1,2,3,4];
+// var arr2 = arr;
+// arr.pop()           //we cannot do like this
+//instead of this we can change state as
+
+arr = [1,2];
+
+console.log(arr);
