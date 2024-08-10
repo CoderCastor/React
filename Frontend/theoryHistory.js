822f12343409fdadb2ea5beb30890d714414f3ca
codercastor <tejasshinde935@gmail.com>
2024-08-10 13:09:16 +0530

    learned Component Reusability

---
+++ Frontend/theoryHistory.js
@@ -0,0 +1,12 @@
+//react is Library
+//not a framework
+//developed in 2013 by facebook engineers
+//get open source in 2015
+
+//problem ---> solution =>REACT
+//when website going through much more changes then all elements get rendered again 
+//it will rerendered those elements which are not changed because of this the entire website gets slow down
+//to avoid this thing REACT gets developed
+
+//hence facebook engineers developed virtual doms
+//virtual dom changing/repainting only those things which are getting changed and not entire website
---
+++ firstProject/src/App.jsx
@@ -0,0 +1,17 @@
+import Card from "./components/Card";
+function App() {
+
+  const data = ["sandip","Kartik","Radha","Satish"];
+
+
+
+
+
+  return (
+    <div>
+      <Card/>
+    </div>
+  )
+}
+
+export default App;
