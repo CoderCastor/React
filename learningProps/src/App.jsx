2957517f61f77494305af88b01dc210641fbae94
codercastor <tejasshinde935@gmail.com>
2024-08-11 21:12:22 +0530

    just commit

--- learningProps/src/App.jsx
+++ learningProps/src/App.jsx
@@ -1,13 +1,15 @@
-import Card from "./Card"
+import Card from "./Card";
 
 function App() {
-
   return (
     <>
-      <Card text={"Download Now"} color={"bg-red-600"}/>
-      <Card text={"Know More"} color={"bg-blue-600"}/>
+      <div className="h-screen w-full flex justify-center items-center">
+        <Card text={"Download Now"} color={"bg-red-600"} />
+        <Card text={"Know More"} color={"bg-blue-600"} />
+        <Card text={"About"} color={"bg-orange-600"} />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- learningProps/src/Card.jsx
+++ learningProps/src/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Card({text,color}) {
   return (
-    <button className={`px-4 py-1 ${color} m-5 rounded-md text-white`}>{text}</button>
+    <button className={`px-5 py-2 ${color} m-2 font-extrabold rounded-full text-white`}>{text}</button>
   )
 }
 
