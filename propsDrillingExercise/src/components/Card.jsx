5a694c3382136836c852e396fe8984b859e6de0e
codercastor <tejasshinde935@gmail.com>
2024-08-12 22:35:18 +0530

    data fetching solved. Done.

--- propsDrillingExercise/src/components/Card.jsx
+++ propsDrillingExercise/src/components/Card.jsx
@@ -9,8 +9,8 @@ function Card({data,handleClick,index}) {
           <img className="w-full h-full object-cover" src={image}/>
         </div>
         <div className="">
-            <h3 className="text-xl leading-none font-semibold">Song Name</h3>
-            <h6 className="text-sm">Artist Name</h6>
+            <h3 className="text-xl leading-none font-semibold">{name}</h3>
+            <h6 className="text-sm mt-3">{artist}</h6>
           </div>
           <button onClick={()=>{handleClick(index)}} className={`px-4 py-3 ${added===false ? "bg-orange-600" : "bg-teal-700"} whitespace-nowrap absolute bottom-0 left-1/2 translate-y-[50%] -translate-x-[50%] text-white text-xs rounded-full`}>{added===false ? "Add to Favorites" : "Added"}</button>
     </div>
