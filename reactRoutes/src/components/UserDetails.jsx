159ce4cabd85a51be076071193ea5c5e05aa4201
codercastor <tejasshinde935@gmail.com>
2024-08-16 21:45:50 +0530

    added go Home Route

--- reactRoutes/src/components/UserDetails.jsx
+++ reactRoutes/src/components/UserDetails.jsx
@@ -4,6 +4,7 @@ import { useNavigate, useParams } from 'react-router-dom'
 function UserDetails() {
 
     const { name } = useParams();
+
     
     const navigate = useNavigate()
     const GoBackHandler = ()=>{
@@ -12,6 +13,9 @@ function UserDetails() {
 
     }
     
+    const GoHomeHandler = ()=>{
+        navigate("/");
+    }
     
 
   return (
@@ -19,6 +23,8 @@ function UserDetails() {
         <h1 className='text-5xl font-semibold'>User Details</h1>
         <h1 className='text-3xl'>Welcome {name}</h1>
         <button onClick={GoBackHandler} className='px-5 py-3 bg-red-500 rounded-lg font-bold text-white'>Go Back</button>
+        <button onClick={GoHomeHandler} className='px-5 py-3 bg-blue-500 rounded-lg font-bold text-white'>Go Home</button>
+
     </div>
     
   )
