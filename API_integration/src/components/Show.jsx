e6b487a52b9b2ee405bc3fc0a196d08c49654473
codercastor <tejasshinde935@gmail.com>
2024-08-18 23:57:34 +0530

    Products Card created

--- API_integration/src/components/Show.jsx
+++ API_integration/src/components/Show.jsx
@@ -10,7 +10,7 @@ function Show() {
     axios
       .get(api)
       .then((products) => {
-        // console.log(products);
+        console.log(products);
         setProducts(products.data);
       })
       .catch((error) => console.log(error));
@@ -39,16 +39,34 @@ function Show() {
       </div>
 
       <hr className="w-full my-8" />
-      <ul>
+      <ul className="flex flex-wrap gap-10 items-center justify-center">
         {products.length > 0 ? (
           products.map((item, index) => (
-            <li key={index} className="rounded-md mb-2 p-5 bg-red-200">
-              {item.title}
-            </li>
+            <li key={index} className="w-80 h-48 bg-zinc-300 shadow-xl overflow-hidden border-2">
+            <div>
+                <div className="w-full flex bg-green-500 justify-between">
+                    <div className="w-full bg-zinc-100 p-3 flex flex-col justify-between">
+                        <div className="font-semibold overflow-hidden h-20">{item.title}</div>
+                        <div className="flex gap-3 bg-red-900 text-white px-4 font-semibold">Price : <nav>{item.price}</nav></div>
+                    </div>
+                    <div className="w-[50%] h-32 bg-white flex justify-center items-center p-4">
+                        <img className="w-full h-full object-cover" src={item.image}/>
+                    </div>
+                </div>
+                <div className="p-1 text-sm h-42 bg-black text-white">Description : {item.description} </div>
+            </div>
+            <div>
+
+            </div>
+        </li>
           ))
         ) : (
           <h1>Loading...</h1>
         )}
+
+        
+
+        
       </ul>
     </div>
   );
